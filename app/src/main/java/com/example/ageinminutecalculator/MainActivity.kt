ec5a682bc25b2b22984bfd068e2476e9a7441ec3
Ankit <ankitthakurniec@gmail.com>
2023-08-19 00:42:45 +0530

    second commit

--- app/src/main/java/com/example/ageinminutecalculator/MainActivity.kt
+++ app/src/main/java/com/example/ageinminutecalculator/MainActivity.kt
@@ -1,11 +1,39 @@
 package com.example.ageinminutecalculator
 
+import android.app.DatePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var button:Button = findViewById(R.id.btn)
+        var date:TextView = findViewById(R.id.date)
+
+        button.setOnClickListener{
+            val c = Calendar.getInstance()
+            val year = c.get(Calendar.YEAR)
+            val month = c.get(Calendar.MONTH)
+            val day = c.get(Calendar.DAY_OF_MONTH)
+
+            val datePickerDialog = DatePickerDialog(
+                this,
+                { view, year, monthOfYear, dayOfMonth ->
+                    date.text =
+                        (dayOfMonth.toString() + "-" + (monthOfYear + 1) + "-" + year)
+                },
+                year,
+                month,
+                day
+            )
+            datePickerDialog.show()
+        }
     }
+
 }
